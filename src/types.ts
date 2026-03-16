318c72e23de6b06eeebdfcb3a5a9d138df01a2a1
Matheus Nobre <matheusnsenado6@gmail.com>
2026-03-16 16:38:49 -0300

    atualizando api redux

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Produto = {
+  id: number
+  nome: string
+  preco: number
+  imagem: string
+}
+export type favoritos = {
+  id: number
+  nome: string
+  preco: number
+  imagem: string
+}
